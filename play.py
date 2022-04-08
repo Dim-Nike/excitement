f6281d44b65857835d0ccc7328cfe6d9ff9e6f7b
sno-is <sno-is@mail.ru>
2022-04-08 21:33:59 +0300

    create 4 combination

--- play.py
+++ play.py
@@ -36,19 +36,62 @@ class Person:
                 self.total_without_suit_card.append(mean)
 
     def examination_pair(self):
+        pari = 0
+        two_pari = 0
+        set = 0
+        full_house = 0
+        kare = 0
+
+        count_pari = 0
+        count_set = 0
         local_list = []
         result = Counter(self.total_without_suit_card)
-        print(result)
+        # print(result)
         for key in result:
-            if result.get(key) == 2:
-                print(f'У меня пара из {key}')
-                self.points = 1
-            if result.get(key) == 3:
-                print(f'У меня сет из {key}')
-                self.points = 4
             if result.get(key) == 4:
+                local_list.append('Карэ')
                 print(f'У меня карэ из {key}')
-                self.points = 15
+                kare += 1
+            if result.get(key) == 3:
+                local_list.append('Сет')
+                print(f'У меня сет из {key}')
+                set = 1
+            elif result.get(key) == 2:
+                local_list.append('Пара')
+                print(f'У меня пара из {key}')
+                pari = 1
+
+        print(local_list)
+        for combination in local_list:
+            if combination == 'Пара':
+                count_pari += 1
+            if combination == 'Сет':
+                count_set += 1
+
+        if count_set == 1 and count_pari == 1 or count_set == 1 and count_pari == 2:
+            full_house += 1
+            print('У меня фулл хаус')
+        if count_pari == 2 or count_pari == 3:
+            two_pari += 1
+            print('У меня две пары!')
+
+        if kare == 1:
+            self.points = 7
+        elif full_house:
+            self.points = 6
+        elif set == 1:
+            self.points = 3
+        elif two_pari:
+            self.points = 2
+        elif pari == 1:
+            self.points = 1
+        print(f'Я {self.name} у меня {self.points} баллов!')
+
+
+
+
+
+
 
         # for count in result:
         #     local_list.append(result[count])
