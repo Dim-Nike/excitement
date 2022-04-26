20a5183404268eef0a08651537ae18d0830ff569
Dmitry_Ubuntu <nikitin_dima2000dn@mail.ru>
2022-04-26 21:29:55 +0300

    create main user

--- chips.py
+++ chips.py
@@ -6,12 +6,24 @@ class Chips:
         self.highest_score = 0
         self.number_of_wins = 0
         self.max_bid = 0
+        self.users_wins = []
 
-    def highest_mark(self, point, room):
-        self.points.append(point.points)
-        if len(self.points) == len(room.number_person):
-            self.highest_score = max(self.points)
-            print(f'---Наивысший балл - {self.highest_score}---')
+    def check_win(self, room):
+        for user in room.number_person:
+            self.points.append(user.points)
+
+        score_max = max(self.points)
+        print(f'Всего баллов - {self.points}')
+        print(f'Наивысший балл - {score_max}')
+
+        for win_user in room.number_person:
+            if score_max <= win_user.points:
+                self.users_wins.append(win_user)
+        print(f'Всего победителей - {len(self.users_wins)}')
+
+        for user in self.users_wins:
+            user.chips += self.total_bank // len(self.users_wins)
+            print(f'{user.name} получил {self.total_bank // len(self.users_wins)} фишек!')
 
     def bid_check(self, user, number_person, happening=1, higher_rate_user=0):
         highest_bid = 0
@@ -31,24 +43,15 @@ class Chips:
             if user.bid > highest_bid:
                 return True
         if happening == 4:
-            if user.chips < highest_bid:
+            if user.chips <= highest_bid:
                 return True
-
-    def win_check(self, room, user):
-        if self.highest_score == user.points:
-            for value in self.points:
-                if self.highest_score == value:
-                    self.number_of_wins += 1
-            user.chips += self.total_bank // self.number_of_wins
-            print(f'Всего победителей - {self.number_of_wins}\n'
-                  f'Фишки передаются игроку с наивысшим баллом - {self.highest_score}\n'
-                  f'Количество фишек - {self.total_bank}')
-        self.number_of_wins = 0
 
     def remove_values(self):
         self.highest_score = 0
         self.total_bank = 0
         self.number_of_wins = 0
+        self.points = []
+        self.users_wins = []
 
 
 
--- main.py
+++ main.py
@@ -12,7 +12,8 @@ room = Room()
 
 
 list_user_in_play = [
-    'Иван', "Игорь"
+    'Иван', "Игорь", 'Артем'
+
 ]
 
 list_playing = []
@@ -49,29 +50,39 @@ def place_bid(user):
         else:
             action_1 = 'Сравнять'
         action_2 = 'Повысить'
-        action_3 = 'Сбросить'
+        action_3 = 'Поставить все!'
+        action_4 = 'Сбросить'
         print(f'1. {action_1}\n'
               f'2. {action_2}\n'
-              f'3. {action_3}')
+              f'3. {action_3}\n'
+              f'4. {action_4}')
+
         user_action = int(input(f'Твой ход!\n'))
         if user_action == 1:
             if action_1 == 'Пропустить':
                 print(f'{user.name} пропустил ход')
                 print(user)
+
             elif action_1 == 'Сравнять':
                 if chips.bid_check(user=user, number_person=room.number_person, happening=2, higher_rate_user=user.chips):
-                    user.place_bet(chips=chips.bid_check(user=user, number_person=room.number_person, happening=0), room_chips=chips)
+                    user.place_bet(chips=chips.bid_check(user=user, number_person=room.number_person, happening=0) - user.bid, room_chips=chips)
+                    print(f'{user.name} поставил {user.bid}')
                     print(user)
                 else:
                     print(f'У {user.name} недостаточно фишек!\nИгрок поставил - {user.bid}\nУ игрока - {user.chips} фишек')
                     bid = user.chips
                     user.place_bet(chips=bid, room_chips=chips)
+            elif action_1 == 'Ва-банк!':
+                print(f'Игрок поставил все!')
+                bid = user.chips
+                user.place_bet(chips=bid, room_chips=chips)
+                break
             break
         if user_action == 2:
             if action_1 != 'Ва-банк!':
                 while True:
                     higher_rate = int(input('Сколько поставить ставку?\n'))
-                    if user.chips > higher_rate:
+                    if user.chips >= higher_rate:
                         if user.bid < higher_rate:
                             if chips.bid_check(user=user, number_person=room.number_person, happening=2, higher_rate_user=higher_rate):
                                 user.place_bet(chips=higher_rate, room_chips=chips)
@@ -94,8 +105,13 @@ def place_bid(user):
             else:
                 print('Недостаточно фишек!')
                 continue
-
         if user_action == 3:
+            bid = user.chips
+            user.place_bet(chips=bid, room_chips=chips)
+            print(f'Игрок поставил {bid} фишек')
+            break
+
+        if user_action == 4:
             user.inPlay = False
             print(F'{user.name} покинул партию!')
             print(user)
@@ -116,10 +132,16 @@ def user_place_bet(room):
         print(user_play)
         print(f'Карты стола - {BigTable.table_card}')
         place_bid(user=user_play)
-        # for user in room.number_person:
-        #     if not chips.bid_check(user=user, number_person=room.number_person, happening=1):
-        #         place_bid(user)                !!!!!!!!!!Разобраться с этим!!!!!!!!!!
         pr_sleep(0)
+    number = 0
+    for user in room.number_person:
+        if not chips.bid_check(user=user, number_person=room.number_person, happening=1):
+            print(user)
+            number = + 1
+            place_bid(user)
+            print(f'Попытка - {number}')
+            pr_sleep(0)
+
     for user_play in room.number_person:
         user_play.bid = 0
 
@@ -149,17 +171,7 @@ def round():
     pr_sleep(0)
     user_place_bet(room=room)
     check_user_room(room=room)
-
-
-
-    for score in room.number_person:
-        if score.inPlay:
-            chips.highest_mark(point=score, room=room)
-
-    for user in room.number_person:
-        print('----------------Я отдаю фишки победителям!!!!!!!!!----------------')
-        chips.win_check(room=room, user=user)
-
+    chips.check_win(room=room)
     for user in list_playing:
         print(f'---{user}---')
     chips.remove_values()
@@ -168,12 +180,12 @@ def round():
 
 
 count = 0
-
+print('--- Начало положено ---')
+print(f'Встречайте наших игроков...\n')
 while True:
     check_user_room(room=room)
     count += 1
-    print('--- Начало положено ---')
-    print(f'Встречайте наших игроков...\n')
+    print(f'Раунд {count}')
     for person in list_playing:
         print(f'Я {person.name}, мое состояние в комнате - {person.inPlay}\nМои фишки - {person.chips}')
     if count >= 4:
@@ -182,6 +194,80 @@ while True:
     round()
     pr_sleep(5)
     room.check_room(list_users=list_playing)
+
+
+# from termcolor  import cprint
+#
+# list_components_for_plane = {'Полетный контроллер': 50,
+#                    'Бесколекторный двигатель': 23,
+#                    'Пропеллер 5.8дм': 17,
+#                    'Видеопередатчик': 30,
+#                    'PV видеокамера': 35,
+#                    'GPS модуль': 40,
+#                    'Регулятор оборотов': 13,
+#                    'Сервы': 30,
+#                    'Литионовые батареи': 7,
+#                    'Пульт управления': 0,
+#                    'Антена': 10,
+#                    'Приемник радиоуправления': 45,
+#                    'Пищалка': 5}
+#
+# list_components_for_cv = {'Полетный контроллер': 50,
+#                    'Бесколекторный двигатель': 20*4,
+#                    'Рама': 30,
+#                    'Видеопередатчик': 30,
+#                    'PV видеокамера': 35,
+#                    'GPS модуль': 30,
+#                    'Регулятор оборотов': 13*4,
+#                    'Батарея': 7,
+#                    'Пульт управления': 0,
+#                    'Антена': 10,
+#                    'Приемник радиоуправления': 45,
+#                    'Пищалка': 5,
+#                    'Пропеллер': 10}
+#
+#
+# all_price_for_cv = 0
+# dollar = 74.8
+# slide = '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+#
+# def plane():
+#     all_price_for_plane = 0
+#     for element, value in list_components_for_plane.items():
+#         all_price_for_plane += value
+#         if value >= 40:
+#             cprint(f'{element} - {value}$', 'red')
+#         elif value >= 20:
+#             cprint(f'{element} - {value}$', 'yellow')
+#         else:
+#             cprint(f'{element} - {value}$', 'green')
+#     print(slide)
+#     print(f'Всего - {all_price_for_plane * dollar} Р ')
+#     return all_price_for_plane * dollar
+#
+#
+# def cv():
+#     all_price_for_plane = 0
+#     for element, value in list_components_for_cv.items():
+#         all_price_for_plane += value
+#         if value >= 40:
+#             cprint(f'{element} - {value}$', 'red')
+#         elif value >= 20:
+#             cprint(f'{element} - {value}$', 'yellow')
+#         else:
+#             cprint(f'{element} - {value}$', 'green')
+#     print(slide)
+#     print(f'Всего - {all_price_for_plane * dollar} Р ')
+#     return all_price_for_cv * dollar
+#
+# plan = plane()
+# cvv = cv()
+# print(slide*2)
+# if plan > cvv:
+#     cprint(f'Самолет дороже на {plan - cvv}', 'cyan')
+# if cvv > plan:
+#     cprint(f'Квадрик дороже на {cvv - plan}', 'cyan')
+
 
 
 
